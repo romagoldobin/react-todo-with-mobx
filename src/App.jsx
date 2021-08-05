93f3b7018e63fa451f0a30ce77b1dcc208c87275
Roman Goldobin <rgoldobin@appliedtech.ru>
2021-08-05 18:20:44 +0500

    WIP

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-/* eslint-disable no-useless-concat */
 import React from 'react';
+import PropTypes from 'prop-types';
 import './App.css';
+
 import {
   AppBar,
   Box,
@@ -18,6 +19,39 @@ import {
 } from '@material-ui/core';
 import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
 
+const ToDoItem = () => (
+  <Card className="ToDo__Item" variant="outlined">
+    <CardContent className="ToDo__Text">
+      <Typography component="p">
+        toDotoDotoDotoDotoDotoDotoDotoDotoDotoDotoDotoDotoDotoDotoDotoDo
+      </Typography>
+    </CardContent>
+    <CardActions>
+      <Checkbox color="secondary" size="small" inputProps={{ 'aria-label': 'to do checked/unchecked' }} />
+      <IconButton color="secondary">
+        <DeleteOutlineIcon fontSize="small" />
+      </IconButton>
+    </CardActions>
+  </Card>
+);
+
+const ToDoList = ({ children }) => (
+  <Box className="ToDo__Body">
+    {children}
+  </Box>
+);
+
+const ToDoForm = () => (
+  <footer className="ToDo__Footer">
+    <form className="Form">
+      <TextField className="TextField FullWidth" placeholder="Type your ToDo here..." color="primary" variant="standard" />
+      <Button className="Button" variant="contained" type="submit" color="primary">
+        Add ToDo
+      </Button>
+    </form>
+  </footer>
+);
+
 const App = () => (
   <>
     <AppBar color="primary">
@@ -28,35 +62,18 @@ const App = () => (
       </Toolbar>
     </AppBar>
     <Container className="Container" maxWidth="xs">
-      <Box className="Wrapper">
-        <Paper className="ToDo">
-          <Box className="ToDo__Body">
-            <Card className="ToDo__Item" variant="outlined">
-              <CardContent className="ToDo__Text">
-                <Typography component="p">
-                  toDotoDotoDotoDotoDotoDotoDotoDotoDotoDotoDotoDotoDotoDotoDotoDo
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Checkbox color="secondary" size="small" inputProps={{ 'aria-label': 'to do checked/unchecked' }} />
-                <IconButton color="secondary">
-                  <DeleteOutlineIcon fontSize="small" />
-                </IconButton>
-              </CardActions>
-            </Card>
-          </Box>
-          <footer className="ToDo__Footer">
-            <form className="Form">
-              <TextField className="TextField FullWidth" placeholder="Type your ToDo here..." color="primary" variant="standard" />
-              <Button className="Button" variant="contained" type="submit" color="primary">
-                Add ToDo
-              </Button>
-            </form>
-          </footer>
-        </Paper>
-      </Box>
+      <Paper className="ToDo">
+        <ToDoList>
+          <ToDoItem />
+        </ToDoList>
+        <ToDoForm />
+      </Paper>
     </Container>
   </>
 
 );
+
+ToDoList.propTypes = {
+  children: PropTypes.element.isRequired,
+};
 export default App;
